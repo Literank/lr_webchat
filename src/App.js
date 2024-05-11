d01bc1c9a08522d8c69a4ad7f1354b1b407ddf16
netdong-li <east2050@gmail.com>
2024-05-11 16:19:50 +0800

    Add login form and socket.io client

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Contact from "./components/contact";
 import Message from "./components/message";
 import TitleBar from "./components/ttile-bar";
+import LoginForm from "./components/login-form";
+import clsx from "clsx";
+import io from "socket.io-client";
 
 function App() {
+  const [logged, setLogged] = useState(false);
+  const [user, setUser] = useState({ emoji: "", name: "" });
+  const [conn, setConn] = useState(null);
   const messages = [
     {
       username: "Alice",
@@ -40,50 +47,74 @@ function App() {
       message: "How's your assignment? I didn't do much yesterday",
     },
   ];
+  useEffect(() => {
+    const socket = io("http://localhost:4000");
+    socket.on("error", (error) => {
+      console.error("Socket error:", error);
+    });
+    setConn(socket);
+  }, []);
+
+  const login = (emoji, name) => {
+    setUser({ emoji, name });
+    setLogged(true);
+  };
   return (
     <div className="app">
-      <h1 className="app-name">Literank Web Chat</h1>
-      <div className="segments">
-        <span className="segment left-seg picked">Chat</span>
-        <span className="segment right-seg">Groups</span>
-      </div>
-      <div className="card">
-        <div className="contacts">
-          {contacts.map((e) => (
-            <Contact
-              username={e.username}
-              message={e.message}
-              isOffline={e.username.includes("Emily")}
-            />
-          ))}
-        </div>
-        <div className="main">
-          <TitleBar username={contacts[0].username} />
-          <div className="messages">
-            {messages.map((e) => (
-              <Message
-                username={e.username}
-                message={e.message}
-                isSelf={e.username === "Bob"}
-              />
-            ))}
+      <h1 className={clsx("app-name", { "center-name": !logged })}>
+        Literank Web Chat
+      </h1>
+      {!logged ? (
+        <LoginForm callback={login} />
+      ) : (
+        <>
+          <div className="segments">
+            <span className="segment left-seg picked">Chat</span>
+            <span className="segment right-seg">Groups</span>
           </div>
-          <div className="edit">
-            <textarea className="edit-box" placeholder="Type here" />
-            <div className="buttons">
-              <button className="send-btn">Send</button>
-              <span className="tip">Ctrl+Enter to send</span>
+          <div className="card">
+            <div className="contacts">
+              {contacts.map((e) => (
+                <Contact
+                  username={e.username}
+                  message={e.message}
+                  isOffline={e.username.includes("Emily")}
+                />
+              ))}
+            </div>
+            <div className="main">
+              <TitleBar username={contacts[0].username} />
+              <div className="messages">
+                {messages.map((e) => (
+                  <Message
+                    username={e.username}
+                    message={e.message}
+                    isSelf={e.username === "Bob"}
+                  />
+                ))}
+              </div>
+              <div className="edit">
+                <textarea className="edit-box" placeholder="Type here" />
+                <div className="buttons">
+                  <button className="send-btn">Send</button>
+                  <span className="tip">Ctrl+Enter to send</span>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-      <div className="status">
-        <span>🐱 Bob</span>
-        <div className="connection-status">
-          <span className="dot connected"></span>
-          <span>Connected</span>
-        </div>
-      </div>
+          <div className="status">
+            <span>
+              {user.emoji} {user.name}
+            </span>
+            <div className="connection-status">
+              <span
+                className={clsx("dot", { connected: conn?.connected })}
+              ></span>
+              <span>{conn?.connected ? "Connected" : "Disconnected"}</span>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
---
+++ src/components/login-form.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import "./Login.css";
+
+const emojis = [
+  "🐨", // Koala
+  "🐒", // Monkey
+  "🐼", // Panda
+  "🐘", // Elephant
+  "🐬", // Dolphin
+  "🐻", // Bear
+  "🐶", // Dog
+  "🐱", // Cat
+  "🐭", // Mouse
+  "🐰", // Rabbit
+  "🦊", // Fox
+  "🦁", // Lion
+  "🐯", // Tiger
+  "🐮", // Cow
+  "🐗", // Boar
+  "🐴", // Horse
+  "🐑", // Sheep
+  "🐺", // Wolf
+  "🐹", // Hamster
+  "🐻‍❄️", // Polar bear
+  "🦝", // Raccoon
+  "🐧", // Penguin
+  "🦥", // Sloth
+  "🦘", // Kangaroo
+  "🦄", // Unicorn
+];
+const LoginForm = (props) => {
+  const [emoji, setEmoji] = useState("");
+  const [name, setName] = useState("");
+  const login = () => {
+    if (!emoji) {
+      alert("Emoji needed.");
+      return;
+    }
+    if (!name) {
+      alert("Name needed.");
+      return;
+    }
+    props.callback(emoji, name);
+  };
+  return (
+    <div className="login-form">
+      <div>
+        <div className="form-row">
+          <label>Emoji</label>
+          <select
+            className="input-box"
+            value={emoji}
+            onChange={(e) => setEmoji(e.target.value)}
+          >
+            <option value="" disabled>
+              Pick Your Emoji
+            </option>
+            {emojis.map((e, i) => (
+              <option className="input-option" key={i} value={e}>
+                {e}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="form-row">
+          <label>Name</label>
+          <input
+            className="input-box"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            onKeyUp={(e) => {
+              if (e.key === "Enter") {
+                login();
+              }
+            }}
+          />
+        </div>
+      </div>
+      <div className="buttons">
+        <button className="send-btn" onClick={login}>
+          Login
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
